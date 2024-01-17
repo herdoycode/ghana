56ccc1df49c34fcbb20c835bf7bbb18d10d2e80c
Herdoy Almamun <herdoy1@gmail.com>
2024-01-17 15:49:26 +0600

    Complete topbar

---
+++ app/components/topbar/Topbar.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import { MdLocationPin } from "react-icons/md";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { MdMail } from "react-icons/md";
+import "./Topbar.scss";
+
+const Topbar = () => {
+  return (
+    <div className="topbar-wrapper">
+      <div className="container">
+        <div className="topbar">
+          <div className="left">
+            <div className="item">
+              <div className="icon">
+                <MdLocationPin />
+              </div>
+              <p className="text">Winston Salem, North Carolina.</p>
+            </div>
+            <div className="item">
+              <div className="icon">
+                <MdMail />
+              </div>
+              <p className="text">bridgeinthegap23@gmail.com</p>
+            </div>
+          </div>
+          <div className="right">
+            <div className="item">
+              <Link href="/">
+                <FaFacebookF />
+              </Link>
+            </div>
+            <div className="item">
+              <Link href="/">
+                <FaInstagram />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
