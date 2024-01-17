61379142e3bed1808472c7865d798aba64146768
Herdoy Almamun <herdoy1@gmail.com>
2024-01-17 17:01:15 +0600

    Add links

--- app/components/topbar/Topbar.tsx
+++ app/components/topbar/Topbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { MdLocationPin } from "react-icons/md";
 import { FaFacebookF } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
+import { IoLogoLinkedin } from "react-icons/io";
 import { MdMail } from "react-icons/md";
 import "./Topbar.scss";
 
@@ -15,7 +16,7 @@ const Topbar = () => {
               <div className="icon">
                 <MdLocationPin />
               </div>
-              <p className="text">Winston Salem, North Carolina.</p>
+              <p className="text">Winston-Salem, North Carolina.</p>
             </div>
             <div className="item">
               <div className="icon">
@@ -26,13 +27,18 @@ const Topbar = () => {
           </div>
           <div className="right">
             <div className="item">
-              <Link href="/">
+              <Link href="https://www.facebook.com/bridgeinthegapworldwide?mibextid=ZbWKwL">
                 <FaFacebookF />
               </Link>
             </div>
             <div className="item">
-              <Link href="/">
+              <Link href="https://www.instagram.com/bridgeinthegap23?igsh=ZHp6ZmdxdTlseHdj">
                 <FaInstagram />
+              </Link>
+            </div>
+            <div className="item">
+              <Link href="https://www.linkedin.com/company/bridge-in-the-gap-worldwide">
+                <IoLogoLinkedin />
               </Link>
             </div>
           </div>
