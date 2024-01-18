bf8e5e0bc565e84af24043d9c553bb582f2bd73f
Herdoy Almamun <herdoy1@gmail.com>
2024-01-18 11:45:09 +0600

    Fix navbar and add slider

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,76 +1,50 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { FaHeart } from "react-icons/fa";
-import { MdClose, MdOutlineMenu } from "react-icons/md";
+import { MdClose } from "react-icons/md";
+import { RxHamburgerMenu } from "react-icons/rx";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  const currentPath = usePathname();
-  const [active, setActive] = useState<boolean>(false);
-
-  const navItems = [
-    { id: 1, label: "Home", link: "/" },
-    { id: 2, label: "About", link: "/about" },
-    { id: 3, label: "Gallery", link: "/gallery" },
-    { id: 4, label: "Contact", link: "/contact" },
-  ];
-
+  const [navActive, setNavActive] = useState<boolean>(false);
   return (
     <div className="nav-wrapper">
       <div className="container">
-        <div className="nav">
+        <div className="navbar">
           <div className="left">
-            <Link href="/" className="logo">
-              <Image src="/logo.png" width={60} height={60} alt="logo" />
-            </Link>
-            <div onClick={() => setActive(!active)} className="toggle-icon">
-              {active ? <MdClose /> : <MdOutlineMenu />}
+            <div className="logo">
+              <Image width={50} height={50} src="/logo.png" alt="logo" />
             </div>
-            <nav className={active ? "active" : ""}>
-              <ul>
-                {navItems.map((item) => (
-                  <li key={item.id}>
-                    <Link
-                      className={
-                        currentPath === item.link
-                          ? "nav-link active"
-                          : "nav-link"
-                      }
-                      href={item.link}
-                    >
-                      {item.label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
+            <div onClick={() => setNavActive(!navActive)} className="toggler">
+              {navActive ? <MdClose /> : <RxHamburgerMenu />}
+            </div>
+            <div className={navActive ? "menu active-menu" : "menu"}>
+              <div className="item">
+                <Link href="/">Home</Link>
+              </div>
+              <div className="item">
+                <Link href="/home">Home</Link>
+              </div>
+              <div className="item">
+                <Link href="/gallery">Gallery</Link>
+              </div>
+              <div className="item">
+                <Link href="/contact">Contact</Link>
+              </div>
+            </div>
           </div>
           <div className="right">
-            <nav>
-              <ul>
-                <li>
-                  <Link
-                    className={
-                      currentPath === "/login" ? "nav-link active" : "nav-link"
-                    }
-                    href="/login"
-                  >
-                    Login
-                  </Link>
-                </li>
-                <li>
-                  <div className="donate-now">
-                    <Link href="/">Donate now</Link>
-                    <div className="icon">
-                      <FaHeart />
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </nav>
+            <div className="item">
+              <Link href="/login">Login</Link>
+            </div>
+            <div className="donate-btn">
+              <span>Donate now</span>
+              <div className="icon">
+                <FaHeart />
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+"use client";
+import AwesomeSlider from "react-awesome-slider";
+import "react-awesome-slider/dist/styles.css";
+import "./page.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <AwesomeSlider>
+        <div className="item" data-src="/1.jpg" />
+        <div className="item" data-src="/1.jpg" />
+        <div className="item" data-src="/1.jpg" />
+      </AwesomeSlider>
+    </div>
+  );
 };
 
 export default Home;
