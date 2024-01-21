e399de5bd49d6d600133db2ce4de1b602a753657
Herdoy Almamun <herdoy1@gmail.com>
2024-01-21 15:24:24 +0600

    Improve ui

--- app/components/slider/Slider.tsx
+++ app/components/slider/Slider.tsx
@@ -1,14 +1,10 @@
 "use client";
-import * as React from "react";
-import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { wrap } from "popmotion";
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import "./Slider.scss";
-const images = [
-  "https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png",
-  "https://d33wubrfki0l68.cloudfront.net/49de349d12db851952c5556f3c637ca772745316/cfc56/static/images/wallpapers/bridge-02@2x.png",
-  "https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png",
-];
+const images = ["/3.jpg", "/1.jpg", "/2.jpg"];
 
 const variants = {
   enter: (direction: number) => {
@@ -77,10 +73,10 @@ const Slider = () => {
       </AnimatePresence>
       <div className="control">
         <div className="next" onClick={() => paginate(1)}>
-          {"‣"}
+          <FaChevronLeft />
         </div>
         <div className="prev" onClick={() => paginate(-1)}>
-          {"‣"}
+          <FaChevronRight />
         </div>
       </div>
     </div>
