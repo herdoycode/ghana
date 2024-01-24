fccff2223ddfa6b23cafa140a2fd45348a1c33ee
= <=>
2024-01-25 01:29:11 +0600

    Create donar section

---
+++ app/components/profileCard/ProfileCard.tsx
@@ -0,0 +1,92 @@
+import Link from "next/link";
+import "./ProfileCard.scss";
+import { CiLinkedin } from "react-icons/ci";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+
+const ProfileCard = () => {
+  return (
+    <div className="donars">
+      <div className="card-wrapper">
+        <div className="card">
+          <div className="card-image">
+            <img src="/22.jpg" alt="profile one" />
+          </div>
+
+          <ul className="social-icons">
+            <li>
+              <Link href="">
+                <i className="fab fa-facebook-f">
+                  <FaFacebookF />
+                </i>
+              </Link>
+            </li>
+            <li>
+              <Link href="">
+                <i className="fab fa-instagram">
+                  <FaInstagram />
+                </i>
+              </Link>
+            </li>
+            <li>
+              <Link href="">
+                <i className="fab fa-twitter">
+                  <CiLinkedin />
+                </i>
+              </Link>
+            </li>
+          </ul>
+
+          <div className="details">
+            <h2>
+              Carolyn Larry
+              <br />
+              <span className="job-title">Donor</span>
+            </h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="card-wrapper">
+        <div className="card profile-two">
+          <div className="card-image profile-img--two">
+            <img src="/11.jpg" alt="profile two" />
+          </div>
+
+          <ul className="social-icons">
+            <li>
+              <Link href="">
+                <i className="fab fa-facebook-f">
+                  <FaFacebookF />
+                </i>
+              </Link>
+            </li>
+            <li>
+              <Link href="">
+                <i className="fab fa-instagram">
+                  <FaInstagram />
+                </i>
+              </Link>
+            </li>
+            <li>
+              <Link href="">
+                <i className="fab fa-twitter">
+                  <CiLinkedin />
+                </i>
+              </Link>
+            </li>
+          </ul>
+
+          <div className="details jane">
+            <h2>
+              Paulette Adams
+              <br />
+              <span className="job-title">Donor</span>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import "./page.scss";
+import ProfileCard from "./components/profileCard/ProfileCard";
 import Slider from "./components/slider/Slider";
 
 const page = () => {
   return (
     <div>
       <Slider />
+      <div className="donor">
+        <h2>Our Main Donors</h2>
+        <ProfileCard />
+      </div>
     </div>
   );
 };
