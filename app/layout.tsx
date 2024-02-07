6fada021a138bf7a593be264c090c3b15b59408d
= <=>
2024-02-08 01:45:28 +0600

    Add google meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7425158339246931"
-          crossOrigin="anonymous"
-        ></script>
+        <meta name="google-adsense-account" content="ca-pub-7425158339246931" />
       </Head>
       <body className={inter.className}>
         <AuthProvider>
