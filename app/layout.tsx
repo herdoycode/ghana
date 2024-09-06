503f45cce90add0ea66c0099858095f97fa996d0
= <=>
2024-09-06 08:20:52 +0600

    Get back

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-// import Footer from "../components/Footer";
-// import Navbar from "../components/Navbar";
-// import Topbar from "../components/Topbar";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Topbar from "../components/Topbar";
 import "./globals.css";
-import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -41,19 +40,10 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Theme>
-            <div className="h-screen flex items-center justify-center">
-              <Image
-                src="/under.png"
-                width={1920}
-                height={1080}
-                alt="under constraction"
-                className="w-full h-auto object-cover"
-              />
-            </div>
-            {/* <Topbar />
+            <Topbar />
             <Navbar />
             {children}
-            <Footer /> */}
+            <Footer />
           </Theme>
         </ThemeProvider>
 
